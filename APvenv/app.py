4571f23fb9ce70cf241aeb4ff792e0c270f98350
mykytynsofia <71774322+mykytynsofia@users.noreply.github.com>
2021-11-13 16:37:02 +0200

    Delete APvenv directory

--- APvenv/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/api/v1/hello-world-7')
-def hello_world():
-    return 'Hello world 7'
--- APvenv/server.py
+++
@@ -1,4 +0,0 @@
-import waitress
-import app
-print("waitress server")
-waitress.serve(app.app, host='0.0.0.0', port=100)
